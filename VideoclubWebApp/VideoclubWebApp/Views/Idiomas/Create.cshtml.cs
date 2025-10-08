f7ea9d47ab70ffc253422e95eb2a2d0c6f337099
MBCX <github.small792@passinbox.com>
2025-10-07 23:34:16 -0400

    Creacion de CRUD para Idiomas

---
+++ VideoclubWebApp/VideoclubWebApp/Views/Idiomas/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideoclubWebApp.Views.Idiomas
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

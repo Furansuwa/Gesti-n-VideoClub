fe8f4f32cd1215886ce91853e5a30041e1f9a385
Francois <f.delacruz26@unapec.edu.do>
2025-11-15 14:52:27 -0400

    problema con el apartado Elenco

--- VideoclubWebApp/VideoclubWebApp/Models/Elenco/Elenco.cs
+++ VideoclubWebApp/VideoclubWebApp/Models/Elenco/Elenco.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VideoclubWebApp.Models.Elenco
 {
+    [Table("Elenco")]
     public class Elenco
     {
         [Key]

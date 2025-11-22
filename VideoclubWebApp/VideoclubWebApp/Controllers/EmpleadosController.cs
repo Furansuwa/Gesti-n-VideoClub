273781b073d87daac3f0f5e2eb59addf32c74a39
Francois <f.delacruz26@unapec.edu.do>
2025-11-22 17:28:21 -0400

    Restriccion de usuario cajero

--- VideoclubWebApp/VideoclubWebApp/Controllers/EmpleadosController.cs
+++ VideoclubWebApp/VideoclubWebApp/Controllers/EmpleadosController.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using VideoClubWebApp.Data;
 using VideoClubWebApp.Models;
 
 namespace VideoclubWebApp.Controllers
+
 {
+    [Authorize(Roles = "Admin")]
     public class EmpleadosController : Controller
     {
         private readonly ApplicationDbContext _context;
